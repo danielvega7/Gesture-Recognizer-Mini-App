557fba9466dc34de355017bb76a0f4d4b503d18d
Daniel Vega <drvega7@icloud.com>
2021-09-26 21:57:36 -0500

    finished gesture recognizer mini app

--- Gesture Recognizer Mini App/ViewController.swift
+++ Gesture Recognizer Mini App/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var ImageView4: UIImageView!
+    @IBOutlet weak var ImageView5: UIImageView!
+    @IBOutlet weak var ImageView6: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func I1Tapped(_ sender: UITapGestureRecognizer) {
+        
+        imageView1.center = sender.location(in: view)
+        
+    }
+    
+    @IBAction func I2Panned(_ sender: UIPanGestureRecognizer) {
+        imageView2.center = sender.location(in: view)
+    }
+    
+    @IBAction func I3Panned(_ sender: UIPanGestureRecognizer) {
+        imageView3.center = sender.location(in: view)
+    }
+    @IBAction func I4Panned(_ sender: UIPanGestureRecognizer) {
+        ImageView4.center = sender.location(in: view)
+    }
+    
+    @IBAction func I5Panned(_ sender: UIPanGestureRecognizer) {
+        ImageView5.center = sender.location(in: view)
+    }
+    
+    @IBAction func I6Panned(_ sender: UIPanGestureRecognizer) {
+        ImageView6.center = sender.location(in: view)
+    }
+    
+    
 }
 
