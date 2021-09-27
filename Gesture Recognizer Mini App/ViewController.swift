102da7c502e6ef0f3371e5562e9ba6c64619d6a0
Daniel Vega <drvega7@icloud.com>
2021-09-26 20:58:08 -0500

    Initial Commit

---
+++ Gesture Recognizer Mini App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gesture Recognizer Mini App
+//
+//  Created by Daniel Vega on 9/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
